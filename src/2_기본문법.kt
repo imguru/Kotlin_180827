011969a9f92cd579cb4c4640ed6f53d8d5bed257
Chansik Yun <chansigi@gmail.com>
2018-08-27 11:59:06 +0900

    .

--- src/2_기본문법.kt
+++ src/2_기본문법.kt
@@ -1,6 +1,7 @@
 package ex3
 
-import ex1.User
+import java.util.*
+
 
 // Java에서 패키지를 사용하기 위해서는 반드시
 // 해당 디렉토리 구조와 동일하게 사용해야 합니다.
@@ -73,7 +74,7 @@ fun main(args: Array<String>) {
 // 6. 변수를 선언하는 방법
 //   * var: 변수
 //   * val: final 변수 (상수) - let
-
+/*
 fun main(args: Array<String>) {
     var user = User("Tom", 42)
     // User user = new User();
@@ -82,6 +83,80 @@ fun main(args: Array<String>) {
     val user2 = User("Tom", 42)
     // final User user2 = new User();
 }
+*/
+
+// 7. 클래스 문법
+/*
+class User1 {
+    val name: String
+    val age: Int
+
+    constructor(name: String, age: Int) {
+        this.name = name
+        this.age = age
+    }
+}
+
+class User2 constructor(val name: String, val age: Int)
+class User3(val name: String, val age: Int)
+
+fun main(args: Array<String>) {
+    val user1 = User1("Tom", 42)
+    val user2 = User2("Tom", 42)
+    val user3 = User3("Tom", 42)
+}
+*/
+
+/*
+// 객체의 equals를 재정의 하였다면, 반드시 hashCode도 재정의 해야 한다.
+//  => Effective Java
+class User(val name: String, val age: Int) {
+    // equals를 재정의하면 됩니다.
+    // @Override
+    // boolean equals(other: Object) {
+    // Any: Object의 타입의 이름이 변경되었습니다.
+    // Any?: Nullable
+    override fun equals(other: Any?): Boolean {
+        if (other === this)
+            return true
+
+        // other: Any?
+        if (other === null)
+            return false
+
+        // other: Any
+
+        // User.class != other.getClass()
+        if (javaClass != other.javaClass)
+            return false
+
+        other as User
+        // other: Any -> User
+        // 여기서부터 other의 타입이 User로 취급됩니다. - 스마트 캐스트
+
+        return name == other.name && age == other.age
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(name, age)
+    }
+}
+
+//  Java: 참조 동등성(==), 객체 동등성(equals)
+//  코틀린: 참조 동등성(===), 객체 동등성 + Null Check(==)
+fun main(args: Array<String>) {
+    val u1 = User("Tom", 42)
+    val u2 = User("Tom", 42)
+
+    println(u1 == u2)
+    println(u1 === u2)
+
+    // Int  타입은 null을 가질 수 없다.
+    // Int? 타입은 null을 가질 수 있다.
+    // val n: Int? = null
+}
+*/
+
 
 
 
--- src/ex1/User.java
+++ src/ex1/User.java
@@ -2,6 +2,8 @@ package ex1;
 
 import ex2.UserKt;
 
+import java.util.Objects;
+
 public class User {
     private String name;
     private int age;
@@ -9,6 +11,29 @@ public class User {
     public User(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // 1. 참조 동등성 체크
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        // 2. 동일한 타입인지 검증
+        //  : Reflection
+        if (User.class != obj.getClass())
+            return false;
+
+        // 3. 내용을 비교
+        User u = (User)obj;
+        return u.age == age &&
+                Objects.equals(u.name, name);
+                // u.name.equals(name);
     }
 
     @Override
