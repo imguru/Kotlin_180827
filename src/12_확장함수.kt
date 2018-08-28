e1c5e3d3833597d734de0568bb6414c3ebd4816c
Chansik Yun <chansigi@gmail.com>
2018-08-28 17:19:03 +0900

    .

--- src/12_확장함수.kt
+++ src/12_확장함수.kt
@@ -1,9 +1,8 @@
 package ex13
+
 // => 코틀린은 JAVA API를 재작성하지 않아도 코틀린이 제공하는 기능을
 //    추가할 수 있었다.
 // => Extension Function
-
-
 
 
 // 확장 함수
@@ -22,6 +21,7 @@ package ex13
 //  5) Kotlin: Extension Function
 
 class User {
+    private var age: Int = 42
     // val name = "Hello"
 
     // Instance method
@@ -36,8 +36,12 @@ class User {
 
 // fun lastChar(text: String): Char = text[text.length - 1]
 
+// * private / protected 는 접근이 불가능하다.
+// fun User.foo(): Int = this.age
 fun String.lastChar(): Char = this[this.length - 1]
 
+
+/*
 fun main(args: Array<String>) {
     val s = "Hello"
     // println(lastChar(s))
@@ -53,13 +57,59 @@ fun main(args: Array<String>) {
     val f2: () -> String = user::name
     println(f2())
 }
+*/
 
+/*
+fun <T> xjoinToString(collection: Collection<T>,
+                     separator: String,
+                     prefix: String,
+                     postfix: String): String {
 
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0)
+            result.append(separator)
 
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+*/
 
+// Collection<String>에 대해서만 확장하고 싶다.
+//fun <T> Collection<T>.xjoinToString(
+//                      separator: String = ", ",
+//                      prefix: String = "[",
+//                      postfix: String = "]"): String {
+fun Collection<String>.xjoinToString(
+        separator: String = ", ",
+        prefix: String = "[",
+        postfix: String = "]"): String {
 
+    val result = StringBuilder(prefix)
+    for ((index, element) in this.withIndex()) {
+        if (index > 0)
+            result.append(separator)
 
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
 
+fun main(args: Array<String>) {
+    val names = listOf("Tom", "Bob", "Alice")
+    val x = listOf(10, 20, 30)
+    val result = names.xjoinToString()
+
+    // x.xjoinToString()
+
+    // names.xjoinToString(", ", "[", "]")
+    // joinToString(names, ", ", "[", "]")
+
+    println(result)
+}
 
 
 
