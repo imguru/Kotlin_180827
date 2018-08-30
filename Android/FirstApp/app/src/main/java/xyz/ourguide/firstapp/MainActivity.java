6025a911e7880d28509322ba8ff438957a34b310
Chansik Yun <chansigi@gmail.com>
2018-08-30 16:15:45 +0900

    .

---
+++ Android/FirstApp/app/src/main/java/xyz/ourguide/firstapp/MainActivity.java
@@ -0,0 +1,35 @@
+package xyz.ourguide.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// FirstApp - Java
+//  Kotlin은 Java와 상호운용성이 뛰어나기 때문에,
+//  혼용해서 사용하는 것이 가능합니다.
+
+// # 기존 안드로이드 프로젝트에 코틀린 지원 추가하는 방법
+//   gradle(groovy 기반)
+//       build.gradle(Project)
+//       build.gradle(Module-App)
+
+// 1. 프로젝트 수준 build.gradle 파일의 classpath에 항목을 추가
+//           classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.60'
+
+// 2. 모듈(앱) 수준 build.gradle
+//    apply plugin: 'kotlin-android'
+
+
+
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
