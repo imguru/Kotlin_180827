e05ff2405698a65b78ebe1f607291c23509004a2
Chansik Yun <chansigi@gmail.com>
2018-08-31 14:14:50 +0900

    .

--- Android/HubClient/app/src/main/java/xyz/ourguide/hubclient/SignInActivity.kt
+++ Android/HubClient/app/src/main/java/xyz/ourguide/hubclient/SignInActivity.kt
@@ -1,10 +1,13 @@
 package xyz.ourguide.hubclient
 
+import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.customtabs.CustomTabsIntent
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 import xyz.ourguide.hubclient.common.GITHUB_CLIENT_ID
 
 
@@ -17,12 +20,18 @@ import xyz.ourguide.hubclient.common.GITHUB_CLIENT_ID
 
 // 3. api.github.com
 //   => 요청을 할 때마다 AccessToken을 HTTP 헤더에 포함해서 요청을 해야 합니다.
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
         signInButton.setOnClickListener { loginWithGithub() }
+    }
+
+    // Activity가 Intent를 받았을 때 호출되는 함수
+    override fun onNewIntent(intent: Intent?) {
+        // github.com이 로그인에 성공하였을 때, 주는 code 값을 읽어야 합니다.
+        info("onNewIntent")
     }
 
     private fun loginWithGithub() {
